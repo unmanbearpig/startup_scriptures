d1d0f06587b044005b52d4a6b1ea083aec5a49cd
unmanbearpig <unmanbearpig@gmail.com>
2014-05-14 20:06:27 +0400

    Added name of current user to header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,10 +7,9 @@ module ApplicationHelper
     }
 
     flash.map do |name, msg|
-      flash_class = flash_classes[name]
+      flash_class = flash_classes[name.to_s]
 
       render partial: 'layouts/flash_message', locals: {name: name, flash_class: flash_class, msg: msg}
     end.join(' ').html_safe
   end
-
 end
