9d8c63280d0e4ff5d386df73f4d0e1c78657fb4e
unmanbearpig <unmanbearpig@gmail.com>
2014-05-14 16:25:49 +0400

    added flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def flash_messages
+    flash_classes = {
+      'notice' => 'alert-success',
+      'alert' => 'alert-warning',
+      'flash' => 'alert-info'
+    }
+
+    flash.map do |name, msg|
+      flash_class = flash_classes[name]
+
+      render partial: 'layouts/flash_message', locals: {name: name, flash_class: flash_class, msg: msg}
+    end.join(' ').html_safe
+  end
 end
