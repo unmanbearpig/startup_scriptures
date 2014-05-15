0fea077e7d3de13a1f4ca0be88ecac424c5f7865
unmanbearpig <unmanbearpig@gmail.com>
2014-05-15 06:14:09 +0400

    added categories edit update and destroy actions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,13 +2,13 @@ class CategoriesController < ApplicationController
   layout 'with_categories_header'
 
   before_action :make_sure_admin_signed_in, except: %i(index show)
+  before_action :find_category, only: %i(show edit update delete)
 
   def index
     render layout: 'with_header'
   end
 
   def show
-    @category = Category.find(params[:id])
   end
 
   def new
@@ -22,15 +22,23 @@ class CategoriesController < ApplicationController
   end
 
   def destroy
+    @category.delete
+    redirect_to categories_path
   end
 
   def edit
   end
 
   def update
+    @category.update(category_params)
+    redirect_to_resource_if_valid @category
   end
 
   private
+
+  def find_category
+    @category = Category.find(params[:id])
+  end
 
   def category_params
     params.require(:category).permit(:name)
