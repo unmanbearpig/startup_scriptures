d605ae2f50b14d68405af7b4458a909a4ab81c0a
unmanbearpig <unmanbearpig@gmail.com>
2014-05-18 01:20:01 +0400

    Fixed subcategory position validation

--- app/models/user_category_subcategory_position.rb
+++ app/models/user_category_subcategory_position.rb
@@ -3,8 +3,7 @@ class UserCategorySubcategoryPosition < ActiveRecord::Base
   belongs_to :category
   belongs_to :subcategory
 
-  validates :category_id, uniqueness: {scope: :user_id}
-  validates :subcategory_id, uniqueness: {scope: [:category_id, :subcategory_id]}
+  validates :subcategory_id, uniqueness: {scope: %i(user_id category_id)}
   validates :position, uniqueness: {scope: [:user_id, :category_id, :subcategory_id]}
 
   acts_as_list
