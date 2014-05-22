1c733396625846a46938bde5300ce3f8845be910
unmanbearpig <unmanbearpig@gmail.com>
2014-05-22 14:19:22 +0400

    Added subcategory_path helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,9 @@ module ApplicationHelper
       content_tag(:h1, title)
     end
   end
+
+  def subcategory_path subcategory
+    category_path(subcategory.category) + "#subcategory-#{subcategory.id}"
+  end
+
 end
