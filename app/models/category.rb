d356d5acebdc9cc1cb3a96a2b7f8cec7fb3de4ec
unmanbearpig <unmanbearpig@gmail.com>
2014-05-22 14:17:56 +0400

    Added category and subcategory to_s method

--- app/models/category.rb
+++ app/models/category.rb
@@ -7,5 +7,7 @@ class Category < ActiveRecord::Base
 
   validates :name, uniqueness: true
 
-
+  def to_s
+    name
+  end
 end
--- app/models/subcategory.rb
+++ app/models/subcategory.rb
@@ -2,4 +2,8 @@ class Subcategory < ActiveRecord::Base
   include BaseCategory
   belongs_to :category
   has_many :links
+
+  def to_s
+    name
+  end
 end
