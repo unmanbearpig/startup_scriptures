aa5183f2d095af38d0c98f85145fd23fcceac878
unmanbearpig <unmanbearpig@gmail.com>
2014-05-29 18:55:11 +0400

    changed mailer url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: 'startup-scriptures-test.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'startupscriptures.herokuapp.com' }
 
   # Settings specified here will take precedence over those in config/application.rb.
 
